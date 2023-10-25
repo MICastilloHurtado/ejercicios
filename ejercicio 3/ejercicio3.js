bcf62f351c1bcdbd14371038636f3a000f00e486
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-10-25 08:14:14 -0500

    cosas nuevas

--- ejercicio 3/ejercicio3.js
+++ ejercicio 3/ejercicio3.js
@@ -1,9 +1,9 @@
 // Crea una funcion que si se le pasa por parametro una palabra que si escrita al revez como escrita normal sigue siendo igual retorne true sino retorar false
 
 const palindromo = (inputString) => {
-    
-    const aux = inputString.split('').reverse().join('')
-
-    return inputString === aux ? true : false
+    for(let i = 0; i<inputString.length; i++){
+        if(inputString[i] == inputString[inputString.length - 1])return true
+        else return false
+    }
 }
 
